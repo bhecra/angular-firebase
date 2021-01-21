dfdb604a7514da7035a809d7875df18ae06b5574
bhecra <ingchristianburbano@gmail.com>
2021-01-20 21:27:22 -0500

    implementación de graficas

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HorizontalBarComponent } from './horizontal-bar/horizontal-bar.component';
 import { RouterModule } from '@angular/router';
-
-
-
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [NavbarComponent, HorizontalBarComponent],
   exports: [NavbarComponent, HorizontalBarComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    NgxChartsModule,
+    BrowserAnimationsModule
   ]
 })
 export class ComponentsModule { }
